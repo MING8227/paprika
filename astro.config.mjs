738ead7de92cca935412395d348c3f9cc8949910
Paprika <your-email@example.com>
2026-03-09 00:08:30 +0800

    Day Zero

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ming8227.github.io',
+  base: '/paprika',
+});
